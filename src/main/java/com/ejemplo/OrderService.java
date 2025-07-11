619c781012602605da9c6bd14611c71e650dca36
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-10 21:38:49 -0400

    arreglos

--- src/main/java/com/ejemplo/OrderService.java
+++ src/main/java/com/ejemplo/OrderService.java
@@ -1,12 +1,17 @@
 package com.ejemplo;
 
 public class OrderService {
-    public double getTotal(double subtotal, boolean hasDiscount, boolean hasExpressShipment) {
+    private DiscountService discountService;
+    public OrderService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
+    public double getTotal(double subtotal, String discountCode, boolean hasExpressShipment) {
         if (subtotal < 0) {
             throw new IllegalArgumentException("Subtotal no puede ser negativo");
     }
 
-        double discount = hasDiscount ? 0.10 : 0;
+        double discount = discountService.getRate(discountCode);
         double shipment = hasExpressShipment ? 20.0 : 10.0;
 
         return(subtotal *(1-discount)) + shipment;
---
+++ src/test/java/com/ejemplo/OrderServiceMockTest.java
@@ -0,0 +1,20 @@
+package com.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class OrderServiceMockTest{
+
+    @Test
+    void testWithMockDiscount() {
+        //Arrange
+        DiscountService buster = mock(DiscountService.class);
+        when(buster.getRate("SALES10")).thenReturn(0.10);
+        OrderService service = new OrderService(buster);
+
+        //Act
+        double total = service.getTotal(100, "SALES10", true);
+        assertEquals(110.0, total);
+    }
+}
