5b62a5e068c9b1bdd0cb297fbc47f9c72310c8fc
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-10 22:41:05 -0400

    agregamos jacoco

--- src/main/java/com/ejemplo/DiscountService.java
+++ src/main/java/com/ejemplo/DiscountService.java
@@ -2,7 +2,9 @@ package com.ejemplo;
 
 public class DiscountService {
     public double getRate(String code) {
-        if (code.equals("SALES10")) return 0.10;
+        if ("SALES10".equals(code)) {
+            return 0.10;
+        }
         return 0.0;
     }
 }
